ec6b4992ac2c467d8f8d6a17a5722bac117e6c5b
Dan <33073652@fsv.cuni.cz>
2023-09-11 15:25:52 +0200

    Documentation strings and presentation

--- Analysis.py
+++ Analysis.py
@@ -3,16 +3,110 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
 def get_OLS_results(dataset, lags, formula = 'delta_midprice ~ OFI', time_int = '30Min', covariation_type = 'HAC'):
+    '''Perform Ordinary Least Squares (OLS) regression on a dataset using a specified formula.
+
+    Parameters
+    ----------
+    dataset : pd.DataFrame
+        A DataFrame containing the data for regression analysis.
+
+    lags : int
+        The maximum number of lags to consider for autocorrelation.
+
+    formula : str, optional
+        The formula for the OLS regression model (default is 'delta_midprice ~ OFI').
+
+    time_int : str, optional
+        The time interval for grouping the data (default is '30Min').
+
+    covariation_type : str, optional
+        The type of covariance estimator for standard errors (default is 'HAC' - Heteroskedasticity and 
+        Autocorrelation Consistent).
+
+    Returns
+    -------
+    pd.Series
+        A Series containing OLS regression results for each time interval, including coefficients,
+        standard errors, and other statistics.
+
+    Description
+    -----------
+    This function performs Ordinary Least Squares (OLS) regression on a dataset using the specified
+    formula. It groups the data by the specified time interval, fits the OLS model for each group,
+    and returns the regression results as a Series.
+
+    Example
+    -------
+    To perform OLS regression on a dataset `df` with a maximum of 2 lags, you can call the function like this:
+    
+    >>> results = get_OLS_results(df, lags=2, formula='delta_midprice ~ OFI', time_int='30Min', covariation_type='HAC')
+    '''
     return dataset.groupby(pd.Grouper(freq = time_int)).apply(lambda dataset: sm.formula.ols(formula, data = dataset)
     .fit(cov_type = covariation_type, cov_kwds={'maxlags':lags}))
 
 def get_beta_coef(OLS_results, Flow_imbalance = 'OFI'):
+    '''Extract beta coefficients from a series of OLS regression results.
+
+    Parameters
+    ----------
+    OLS_results : pd.Series
+        A Series containing OLS regression results, typically obtained from the `get_OLS_results` function.
+
+    Flow_imbalance : str, optional
+        The flow imbalance variable for which beta coefficients should be extracted (default is 'OFI').
+
+    Returns
+    -------
+    pd.Series
+        A Series containing beta coefficients for the specified flow imbalance variable, indexed by time interval.
+
+    Description
+    -----------
+    This function extracts beta coefficients (slope) from a series of OLS regression results for a specific
+    flow imbalance variable. It returns the beta coefficients as a Series, with each coefficient corresponding
+    to a time interval.
+
+    Example
+    -------
+    To extract beta coefficients for the 'OFI' variable from a series of OLS results `results`, you can call
+    the function like this:
+    
+    >>> beta_coefs = get_beta_coef(results, Flow_imbalance='OFI')
+    '''
     aux_list = []
     for i in OLS_results:
         aux_list.append(i.params[Flow_imbalance])
     return pd.Series(aux_list)
 
 def create_dataframe(beta_series, depth_series):
+    '''Create a DataFrame from beta coefficients and average depth series.
+
+    Parameters
+    ----------
+    beta_series : pd.Series
+        A Series containing beta coefficients indexed by time interval.
+
+    depth_series : pd.Series
+        A Series containing average depth values indexed by time interval.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame containing beta coefficients and average depth values, aligned by timestamp.
+
+    Description
+    -----------
+    This function creates a DataFrame by aligning beta coefficients and average depth values based on
+    their timestamps. It ensures that the resulting DataFrame has a common timestamp index for both
+    beta coefficients and average depth values.
+
+    Example
+    -------
+    To create a DataFrame from beta coefficients in `beta_series` and average depth values in `depth_series`,
+    you can call the function like this:
+    
+    >>> df = create_dataframe(beta_series, depth_series)
+    '''
     start_datetime = depth_series.index.min()
     end_datetime = depth_series.index.max()
 
@@ -23,6 +117,50 @@ def create_dataframe(beta_series, depth_series):
     return pd.DataFrame({'beta_coef': beta_series, 'avg_depth': depth_series})    
 
 def finished_df(start_date, end_date, lags = 4, formula = 'delta_midprice ~ OFI', time_int = '30Min', covariation_type = 'HAC', Flow_imbalance='OFI'):
+    '''Generate a DataFrame containing beta coefficients, average depth values, and other metrics for a specified date range.
+
+    Parameters
+    ----------
+    start_date : str
+        The start date in the format 'YYYY-MM-DD'.
+
+    end_date : str
+        The end date in the format 'YYYY-MM-DD'.
+
+    lags : int, optional
+        The maximum number of lags to consider for autocorrelation (default is 4).
+
+    formula : str, optional
+        The formula for the OLS regression model (default is 'delta_midprice ~ OFI').
+
+    time_int : str, optional
+        The time interval for resampling the data (default is '30Min').
+
+    covariation_type : str, optional
+        The type of covariance estimator for standard errors (default is 'HAC' - Heteroskedasticity and 
+        Autocorrelation Consistent).
+
+    Flow_imbalance : str, optional
+        The flow imbalance variable for which beta coefficients should be extracted (default is 'OFI').
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame containing beta coefficients, average depth values, and other metrics for the specified date range.
+
+    Description
+    -----------
+    This function reads data from CSV files, performs OLS regression, extracts beta coefficients, and creates
+    a DataFrame containing beta coefficients, average depth values, and other metrics. The data is filtered
+    to include only the specified date range.
+
+    Example
+    -------
+    To generate a DataFrame for beta coefficients, average depth values, and other metrics from '2023-09-11'
+    to '2023-09-15', you can call the function like this:
+    
+    >>> df = finished_df('2023-09-11', '2023-09-15', lags=4, formula='delta_midprice ~ OFI', time_int='30Min', covariation_type='HAC', Flow_imbalance='OFI')
+    '''
     column_types = {
         'delta_midprice': 'float64',
         'OFI': 'float64',
@@ -44,6 +182,25 @@ def finished_df(start_date, end_date, lags = 4, formula = 'delta_midprice ~ OFI'
         return None
 
 def list_of_halfhour_timestamps():
+    '''Generate a list of timestamps representing half-hour intervals in a 24-hour day.
+
+    Returns
+    -------
+    list
+        A list of timestamps in the format 'HH:MM' representing half-hour intervals in a day.
+
+    Description
+    -----------
+    This function generates a list of timestamps representing half-hour intervals in a 24-hour day.
+    The timestamps are in the 'HH:MM' format, where 'HH' represents the hour and 'MM' represents
+    the minute.
+
+    Example
+    -------
+    To generate a list of half-hour timestamps, you can call the function like this:
+    
+    >>> timestamps = list_of_halfhour_timestamps()
+    '''
     times = []
     for hour in range(24):
         for minute in ['00', '30']:
@@ -52,6 +209,31 @@ def list_of_halfhour_timestamps():
     return times
 
 def get_graph(beta_D_data):
+    '''Generate a line plot showing normalized beta and depth values over half-hour intervals.
+
+    Parameters
+    ----------
+    beta_D_data : pd.DataFrame
+        A DataFrame containing beta coefficients, average depth values, and timestamps.
+
+    Returns
+    -------
+    None
+        Displays a line plot of normalized beta and depth values.
+
+    Description
+    -----------
+    This function takes a DataFrame `beta_D_data` containing beta coefficients, average depth values, and
+    timestamps. It calculates the global means of beta and depth, normalizes the data, and then generates
+    a line plot to visualize the normalized values over half-hour intervals.
+
+    Example
+    -------
+    To generate and display a line plot for normalized beta and depth values from a DataFrame `df`, you can
+    call the function like this:
+    
+    >>> get_graph(df)
+    '''
     halfhour_list = list_of_halfhour_timestamps() 
     beta_D_data['time'] = beta_D_data.index.strftime('%H:%M:%S')
 
--- DataPreparation.py
+++ DataPreparation.py
@@ -2,30 +2,182 @@ import pandas as pd
 from datetime import timedelta, datetime
 
 def load_df_quotes(datum):
+    '''Load a DataFrame containing cryptocurrency quote data from a CSV file.
+
+    Parameters:
+    -----------
+    datum : str
+        The date for which you want to load the quote data in the format 'YYYY-MM-DD'.
+
+    Returns:
+    --------
+    pd.DataFrame
+        A DataFrame containing cryptocurrency quote data with columns for exchange, symbol,
+        timestamp, local timestamp, ask amount, ask price, bid price, and bid amount.
+
+    Example:
+    --------
+    To load quote data for the date '2023-09-11', you can call the function like this:
+    
+    >>> df = load_df_quotes('2023-09-11')
+    '''
     coltypes = {'exchange': str, 'symbol': str, 'timestamp': int, 'local_timestamp': int, 'ask_amount': float, 
                 'ask_price': float, 'bid_price': float, 'bid_amount': float}
     return pd.read_csv(f'/Users/marekerben/Desktop/Prakticka/binance-futures/BTCUSDT/quotes/{datum}.csv.gz', dtype = coltypes, 
             index_col=['timestamp'], parse_dates=['timestamp'], date_parser=lambda d: pd.to_datetime(d, unit = 'us'))    
 
 def load_df_trades(datum):
+    '''Load a DataFrame containing cryptocurrency trade data from a CSV file.
+
+    Parameters
+    ----------
+    datum : str
+        The date for which you want to load the trade data in the format 'YYYY-MM-DD'.
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame containing cryptocurrency trade data with columns for exchange, symbol,
+        timestamp, local timestamp, trade ID, side, price, and amount.
+
+    Example
+    -------
+    To load trade data for the date '2023-09-11', you can call the function like this:
+    
+    >>> df = load_df_trades('2023-09-11')
+    '''
     coltypes = {'exchange': str, 'symbol': str, 'timestamp': int, 'local_timestamp': int, 'id': int, 
                 'side': str, 'price': float, 'amount': float}
     return pd.read_csv(f'/Users/marekerben/Desktop/Prakticka/binance-futures/BTCUSDT/trades/{datum}.csv.gz', dtype = coltypes, 
             index_col=['timestamp'], parse_dates=['timestamp'], date_parser=lambda d: pd.to_datetime(d, unit = 'us'))   
 
 def get_e_n(data_frame):
+    '''Calculate the net exchange flow (e_n) for a given DataFrame of cryptocurrency data.
+
+    Parameters
+    ----------
+    data_frame : pd.DataFrame
+        A DataFrame containing cryptocurrency data with columns for bid price, bid amount, 
+        ask price, and ask amount.
+
+    Returns
+    -------
+    pd.Series
+        A Series representing the net exchange flow (e_n) calculated based on the provided data.
+
+    Description
+    -----------
+    The net exchange flow (e_n) is calculated as the difference between the cumulative bid amount 
+    for price increases and the cumulative ask amount for price decreases at each timestamp. 
+    It helps analyze the flow of orders in the cryptocurrency market.
+
+    Example
+    -------
+    To calculate the net exchange flow (e_n) for a DataFrame `df`, you can call the function like this:
+    
+    >>> en = get_e_n(df)
+    '''
     return (data_frame['bid_price'].diff() >= 0) * data_frame['bid_amount'] - \
         (data_frame['bid_price'].diff() <= 0) * data_frame['bid_amount'].shift(1)- \
         (data_frame['ask_price'].diff() <= 0) * data_frame['ask_amount'] + \
         (data_frame['ask_price'].diff() >= 0) * data_frame['ask_amount'].shift(1)
 
 def get_mid_price(ask_price, bid_price, tick_size = 0.01):
+    '''Calculate the mid price based on the ask and bid prices.
+
+    Parameters
+    ----------
+    ask_price : float
+        The ask price of the cryptocurrency.
+
+    bid_price : float
+        The bid price of the cryptocurrency.
+
+    tick_size : float, optional
+        The tick size used for the mid price calculation (default is 0.01).
+
+    Returns
+    -------
+    float
+        The calculated mid price of the cryptocurrency.
+
+    Description
+    -----------
+    The mid price is calculated as the average of the ask and bid prices, divided by
+    twice the tick size. It represents the central price point between the current
+    best ask and best bid prices in the market.
+
+    Example
+    -------
+    To calculate the mid price for an ask price of 100.0, a bid price of 99.5, and a tick size of 0.01, 
+    you can call the function like this:
+    
+    >>> mid_price = get_mid_price(100.0, 99.5, 0.01)
+    '''
     return (ask_price + bid_price) / (2 * tick_size)
 
 def get_signed_amount_traded(data_frame):
+    '''Calculate the signed amount traded based on the side (buy/sell) in a DataFrame.
+
+    Parameters
+    ----------
+    data_frame : pd.DataFrame
+        A DataFrame containing cryptocurrency trade data with columns for 'side' and 'amount'.
+
+    Returns
+    -------
+    pd.Series
+        A Series representing the signed amount traded, where buy trades have positive values
+        and sell trades have negative values.
+
+    Description
+    -----------
+    This function calculates the signed amount traded for each row in the DataFrame. If the trade
+    side is 'buy,' the amount is positive; if the trade side is 'sell,' the amount is negated
+    to represent a sell trade.
+
+    Example
+    -------
+    To calculate the signed amount traded for a DataFrame `df` containing trade data, you can call
+    the function like this:
+    
+    >>> signed_amount = get_signed_amount_traded(df)
+    '''
     return data_frame.apply(lambda row: row['amount'] if row['side'] == 'buy' else -1 * row['amount'], axis=1)
 
 def construct_OFI_TFI_dataframe(datum, delta_t = '10S', tick_size = 0.01):
+    '''Construct a DataFrame containing Order Flow Imbalance (OFI) and Traded Flow Imbalance (TFI) data.
+
+    Parameters
+    ----------
+    datum : str
+        The date for which you want to construct the OFI and TFI DataFrame in the format 'YYYY-MM-DD'.
+
+    delta_t : str, optional
+        The time interval for resampling the data (default is '10S' - 10 seconds).
+
+    tick_size : float, optional
+        The tick size used for mid price calculation (default is 0.01).
+
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame containing 'delta_midprice' (change in mid price), 'OFI' (Order Flow Imbalance), 
+        and 'TFI' (Traded Flow Imbalance) data resampled at the specified time interval.
+
+    Description
+    -----------
+    This function loads quote and trade data for a specific date, calculates the change in mid price,
+    Order Flow Imbalance (OFI), and Traded Flow Imbalance (TFI) over the specified time intervals, and 
+    constructs a DataFrame with these metrics.
+
+    Example
+    -------
+    To construct an OFI and TFI DataFrame for the date '2023-09-11' with a time interval of '10S', 
+    you can call the function like this:
+    
+    >>> df = construct_OFI_TFI_dataframe('2023-09-11', delta_t='10S', tick_size=0.01)
+    '''
     quotes_df = load_df_quotes(datum)
     trades_df = load_df_trades(datum)
 
@@ -35,6 +187,32 @@ def construct_OFI_TFI_dataframe(datum, delta_t = '10S', tick_size = 0.01):
              'TFI': get_signed_amount_traded(trades_df).resample(delta_t).sum()})
 
 def date_range_list(start_date, end_date):
+    '''Generate a list of dates within a specified date range.
+
+    Parameters
+    ----------
+    start_date : str
+        The start date in the format 'YYYY-MM-DD'.
+
+    end_date : str
+        The end date in the format 'YYYY-MM-DD'.
+
+    Returns
+    -------
+    list
+        A list of date strings within the specified date range.
+
+    Description
+    -----------
+    This function generates a list of dates, including the start date and end date, within the specified
+    date range. The dates are represented as strings in the 'YYYY-MM-DD' format.
+
+    Example
+    -------
+    To generate a list of dates from '2023-09-11' to '2023-09-15', you can call the function like this:
+    
+    >>> date_list = date_range_list('2023-09-11', '2023-09-15')
+    '''
     list_of_dates = []
     start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
     end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
@@ -45,6 +223,41 @@ def date_range_list(start_date, end_date):
     return list_of_dates
 
 def output_df(start_date, end_date, delta_t = '10S', tick_size = 0.01):
+    '''Generate and concatenate Order Flow Imbalance (OFI) and Traded Flow Imbalance (TFI) DataFrames
+    for a specified date range.
+
+    Parameters
+    ----------
+    start_date : str
+        The start date in the format 'YYYY-MM-DD'.
+
+    end_date : str
+        The end date in the format 'YYYY-MM-DD'.
+
+    delta_t : str, optional
+        The time interval for resampling the data (default is '10S' - 10 seconds).
+
+    tick_size : float, optional
+        The tick size used for mid price calculation (default is 0.01).
+
+    Returns
+    -------
+    pd.DataFrame
+        A concatenated DataFrame containing OFI and TFI data for the specified date range.
+
+    Description
+    -----------
+    This function generates OFI and TFI DataFrames for a range of dates within the specified date
+    range. It catches and handles potential errors during the data processing and concatenates the
+    results into a single DataFrame.
+
+    Example
+    -------
+    To generate OFI and TFI DataFrames for the date range from '2023-09-11' to '2023-09-15' with
+    a time interval of '10S' and a tick size of 0.01, you can call the function like this:
+    
+    >>> df = output_df('2023-09-11', '2023-09-15', delta_t='10S', tick_size=0.01)
+    '''
     dates_list = date_range_list(start_date, end_date)
 
     results = []
@@ -60,16 +273,70 @@ def output_df(start_date, end_date, delta_t = '10S', tick_size = 0.01):
     return pd.concat(results)
 
 def get_avg_depth(df):
+    '''Calculate the average depth of the order book from a DataFrame of cryptocurrency quote data.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        A DataFrame containing cryptocurrency quote data with columns for bid price and bid amount,
+        as well as ask price and ask amount.
+
+    Returns
+    -------
+    float
+        The calculated average depth of the order book.
+
+    Description
+    -----------
+    The average depth of the order book is calculated as the average of the sum of bid and ask amounts
+    for price decreases and price increases, respectively, divided by the count of price changes.
+
+    Example
+    -------
+    To calculate the average depth of the order book for a DataFrame `df`, you can call the function like this:
+    
+    >>> avg_depth = get_avg_depth(df)
+    '''
     return 0.5 *(((((df['bid_price'].diff() < 0) * df['bid_amount'] + (df['bid_price'].diff() > 0) * df['bid_amount'].shift(1))).sum()/((df['bid_price'].diff() != 0).sum()) + 
 (((df['ask_price'].diff() > 0) * df['ask_amount'] + (df['ask_price'].diff() < 0) * df['ask_amount'].shift(1))).sum()/((df['ask_price'].diff() != 0).sum())))
 
 def get_all_avg_depths(start_date, end_date, time_int = '30Min'):
+    '''Calculate the average depth of the order book over a specified date range and time interval.
+
+    Parameters
+    ----------
+    start_date : str
+        The start date in the format 'YYYY-MM-DD'.
+
+    end_date : str
+        The end date in the format 'YYYY-MM-DD'.
+
+    time_int : str, optional
+        The time interval for resampling the data (default is '30Min').
+
+    Returns
+    -------
+    pd.Series
+        A Series containing the calculated average depths of the order book, indexed by timestamp.
+
+    Description
+    -----------
+    This function calculates the average depth of the order book for a range of dates within the specified
+    date range and resamples the data at the provided time interval. The results are returned as a Series.
+
+    Example
+    -------
+    To calculate the average depths of the order book from '2023-09-11' to '2023-09-15' with a time
+    interval of '30Min', you can call the function like this:
+    
+    >>> avg_depths = get_all_avg_depths('2023-09-11', '2023-09-15', time_int='30Min')
+    '''
     dates_list = date_range_list(start_date = start_date, end_date = end_date)
     series = pd.concat([load_df_quotes(i).resample(time_int).apply(lambda x: get_avg_depth(x)) for i in dates_list], axis = 0)
     series.name = 'avg_depth'
     return series
 
-def control(df):
+#def control(df):
     '''
     Dodělat je to kontrola jestli máme všechny řádky v našem spočítáném df, tedy jestli ve všech 10s intervalech byli kotace a jsou tam data.
     Stalo se mi, že když nebyli data, tak to ty řádky vynechalo. přes np.where(np.array == False)[0]
